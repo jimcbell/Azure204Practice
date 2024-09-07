abf8560cdbd0c5fc29833f572d68a55cb2cd5a5d
Jim Campbell <jimcampbell355@gmail.com>
2024-09-06 20:26:45 -0400

    Added json file

--- StorageAccountPractice/Program.CreateSasUrlsFromContainer.cs
+++ StorageAccountPractice/Program.CreateSasUrlsFromContainer.cs
@@ -5,7 +5,10 @@ using Microsoft.Extensions.Configuration;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-IConfiguration configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .AddJsonFile("appsettings.json")
+    .Build();
 // Get my storage account connection string.
 string connectionString = configuration["StorageAccountConnectionString"]
     ?? throw new Exception("Secret Management Impoperly Set Up");
