f8a70242e10ceb2a413d4f2f91228e25a4da3d6b
Jim Campbell <jimcampbell355@gmail.com>
2024-09-06 20:13:01 -0400

    Added blob sas builder.

--- StorageAccountPractice/Program.cs
+++ StorageAccountPractice/Program.cs
@@ -1,5 +1,26 @@
 ﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+using Microsoft.Extensions.Configuration;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-BlobContainerClient client = 
+IConfiguration configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+// Get my storage account connection string.
+string connectionString = configuration["StorageAccountConnectionString"] 
+    ?? throw new Exception("Secret Management Impoperly Set Up");
+
+BlobContainerClient client = new(connectionString, "test");
+if (client.CanGenerateSasUri)
+{
+    foreach (BlobItem item in client.GetBlobs())
+    {
+        Console.WriteLine(item.Name);
+        Console.WriteLine("SAS Url: ");
+        // Create a BlobSasBuilder that will give the SAS all permissions expiring in a day.
+        DateTimeOffset expiresOn = DateTimeOffset.Now.AddDays(1);
+        BlobSasBuilder builder = new BlobSasBuilder(permissions: BlobSasPermissions.All, expiresOn: expiresOn);
+        builder.BlobName = item.Name;
+        
+    }
+}
--- StorageAccountPractice/ReadMe.md
+++ StorageAccountPractice/ReadMe.md
@@ -2,3 +2,13 @@ Scripts ran at the creation of the project. For reference:
 
 // To add the package (got this from the Microsoft Learn page: https://learn.microsoft.com/en-us/dotnet/api/overview/azure/storage.blobs-readme?view=azure-dotnet)
 dotnet add package Azure.Storage.Blobs
+
+Add Configuration to not expose my Storage Account connection string :)
+
+```dotnet add package Microsoft.Extensions.Configuration```
+
+Add Secret Manager to the project. This is a good way of storing secrets s.t. you do not need 
+an appsettings.json, app.Config, etc. and the runtime will manage user secrets during development.
+
+```dotnet user-secrets init```
+
