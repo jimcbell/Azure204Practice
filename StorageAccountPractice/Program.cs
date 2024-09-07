053701625581cab2602f856b57b3ae340a2a1d39
Jim Campbell <jimcampbell355@gmail.com>
2024-09-06 20:24:12 -0400

    SAS urls are working

--- StorageAccountPractice/Program.cs
+++ StorageAccountPractice/Program.cs
@@ -7,20 +7,37 @@ Console.WriteLine("Hello, World!");
 
 IConfiguration configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
 // Get my storage account connection string.
-string connectionString = configuration["StorageAccountConnectionString"] 
+string connectionString = configuration["StorageAccountConnectionString"]
     ?? throw new Exception("Secret Management Impoperly Set Up");
-
+// Not disposable.
 BlobContainerClient client = new(connectionString, "test");
 if (client.CanGenerateSasUri)
 {
     foreach (BlobItem item in client.GetBlobs())
     {
         Console.WriteLine(item.Name);
-        Console.WriteLine("SAS Url: ");
-        // Create a BlobSasBuilder that will give the SAS all permissions expiring in a day.
-        DateTimeOffset expiresOn = DateTimeOffset.Now.AddDays(1);
-        BlobSasBuilder builder = new BlobSasBuilder(permissions: BlobSasPermissions.All, expiresOn: expiresOn);
-        builder.BlobName = item.Name;
-        
+        Uri sasUri = createSasUri(client, item);
+        Console.WriteLine($"SAS Url: {sasUri}");
     }
+}
+
+static BlobSasBuilder createBlobSasBuilder(BlobItem item)
+{
+    // Create a BlobSasBuilder that will give the SAS all permissions expiring in a day.
+    DateTimeOffset expiresOn = DateTimeOffset.Now.AddDays(1);
+    BlobSasBuilder builder = new BlobSasBuilder(permissions: BlobSasPermissions.All, expiresOn: expiresOn)
+    {
+        BlobName = item.Name,
+        Resource = "b"
+    };
+    return builder;
+}
+
+static Uri createSasUri(BlobContainerClient client, BlobItem item)
+{
+    // Not disposable.
+    BlobClient blobClient = client.GetBlobClient(item.Name);
+    BlobSasBuilder builder = createBlobSasBuilder(item);
+    Uri sasUri = blobClient.GenerateSasUri(builder);
+    return sasUri;
 }
