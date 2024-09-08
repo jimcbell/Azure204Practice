04ba0d0e2f78c12441e6581fc67d9cc732d5ed0b
Jim Campbell <jimcampbell355@gmail.com>
2024-09-08 12:51:16 -0400

    Extension method to prevent warnings for nullable types.

---
+++ StorageAccountPractice/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageAccountPractice.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetAppSetting(this IConfiguration config, string key)
+        {
+            string? value = config[key];
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            return value;
+        }
+    }
+}
